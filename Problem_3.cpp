e8d8e2b69d67813f4b576f777a14fca02fba18ee
Adam Porembinski <75131243+plextor15@users.noreply.github.com>
2023-03-29 21:32:29 +0200

    Update Problem_3.cpp

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -4,19 +4,19 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
-
 #include <cstdio>
 
 int main() 
 {
     unsigned long number = 600851475143;
     unsigned long dzielnik = 2;
-    unsigned long tmp = 0;
+    unsigned long wynik;
     
-    while(true){
+    while(true)
+    {
         if(dzielnik * dzielnik > number)
         {
-            tmp = number;
+            wynik = number;
             break;
         }
         
@@ -24,14 +24,13 @@ int main()
         {
             // printf("%ul \n", dzielnik);  //DEBUG ONLY!!
             number = number / dzielnik;
-            // tmp = dzielnik;
         }
-        else{
+        else
+        {
             dzielnik++;
         }
     }
     
-    printf("%ul", tmp);
-
+    printf("%lu", wynik);
     return 0;
 }
